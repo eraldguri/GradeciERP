ccc75997b51b8042b04f826dad2d1c7f650d7a6a
Erald <eraldguri@live.com>
2026-02-16 18:17:26 +0100

    Add project files.

---
+++ Domain/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Country { get; set; }
+    public string? TimeZone { get; set; }
+    public string? Currency { get; set; }
+    public DateTime EstablishedDate { get; set; }
+}
---
+++ Infrastructure/Tenancy/CompanyTenantInfo.cs
@@ -0,0 +1,16 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+
+namespace Infrastructure.Tenancy;
+
+public class CompanyTenantInfo : ITenantInfo
+{
+    public string? Id { get; set ; }
+    public string? Identifier { get; set ; }
+    public string? Name { get; set; }
+    public string? CoonectionString { get; set; }
+    public string? Email {  get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime ValidUpTo { get; set; }
+    public bool IsActive { get; set; }
+}
