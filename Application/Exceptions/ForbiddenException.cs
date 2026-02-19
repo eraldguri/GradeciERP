b848c46deeb2a421e7a130097b2007a6e1dcb510
Erald <eraldguri@live.com>
2026-02-19 14:21:55 +0100

    Custom exceptions and Global Response Wrapper

---
+++ Application/Exceptions/ForbiddenException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Application.Exceptions
+{
+    public class ForbiddenException : Exception
+    {
+        public List<string>? ErrorMessages { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public ForbiddenException(List<string>? errorMessages = default, HttpStatusCode statusCode = HttpStatusCode.Forbidden)
+        {
+            ErrorMessages = errorMessages;
+            StatusCode = statusCode;
+        }
+    }
+}
